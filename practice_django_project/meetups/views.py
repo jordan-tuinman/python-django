8ead467b860758384f6867db4d86c57877fffaa7
jordan-tuinman <jojotuijo@gmail.com>
2021-07-17 12:19:04 +1200

    add styles

--- practice_django_project/meetups/views.py
+++ practice_django_project/meetups/views.py
@@ -3,4 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, 'meetups/index.html')
+  meetups = [
+    {'title': 'A sample meetup', 
+    'location': 'Tokyo', 
+    'slug': 'a-first-meetup'},
+    {'title': 'A second sample meetup', 
+    'location': 'New York', 
+    'slug': 'a-second-meetup'},
+    {'title': 'A third sample meetup', 
+    'location': 'Tokyo', 
+    'slug': 'a-third-meetup'},
+    {'title': 'A fourth sample meetup', 
+    'location': 'Tokyo', 
+    'slug': 'a-fourth-meetup'}
+  ]
+  return render(request, 'meetups/index.html', {
+    'show_meetups': True,
+    'meetups': meetups
+  })
